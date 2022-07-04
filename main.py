0fac39ac818d260e7e592cf3fdefa2be4f124517
Brayan Correa <brayancorrea78@gmail.com>
2022-07-04 08:16:28 -0500

    added query and path validations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Dict, Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query, Path
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,3 +21,42 @@ def home(string) -> Dict:
 @app.post("/user/create")
 def user_create(user: User = Body(...)):
     return user
+
+
+@app.get('/user/detail')
+def user_detail(
+        email: str = Query(
+            min_length=5,
+            max_length=50,
+            title="User email",
+            description="This is the user email. It's between 5 and 50 characters."
+        ),
+        password: str = Query(
+            ...,
+            min_length=6,
+            title="User password",
+            description="This is the user password. Min 6 characters"
+        ),
+        cellphone: Optional[str] = Query(
+            None,
+            min_length=10,
+            max_length=10,
+            title="User cellphone",
+            description="This is the user password. Min 10 characters"
+        )
+):
+    return {
+        "email": email,
+        "password": password,
+        "cellphone": cellphone
+    }
+
+
+@app.get("/user/detail/{user_id}")
+def user_detail_by_id(
+        user_id: int = Path(
+            ...,
+            gt=0
+        )
+):
+    return {user_id: "It exists!"}
